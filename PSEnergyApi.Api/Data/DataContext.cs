d066f912fce262e43c89ebad6595dee4e916a7b6
LuisPulenta <luisalbertonu@gmail.com>
2021-12-18 15:47:14 -0300

    UsuariosController

---
+++ PSEnergyApi.Api/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PSEnergyApi.Web.Data.Entities;
+
+namespace PSEnergyApi.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ PSEnergyApi.Api/Data/Entities/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PSEnergyApi.Web.Data.Entities
+{
+    public class Usuario
+    {
+        [Key]
+        public int IDUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Login { get; set; }
+        public string Contrasena { get; set; }
+        public DateTime? FechaUltimoAcceso { get; set; }
+        [Display(Name = "Usuario")]
+        public string FullName => $"{Nombre} {Apellido}";
+    }
+}
